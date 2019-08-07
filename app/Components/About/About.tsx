0935cd5daf90f2fee0925a7629f55b121d4f0966
Unknown <gali1998@gmail.com>
2019-08-07 10:37:53 +0300

    initial project

---
+++ app/Components/About/About.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export const About = () => { return (
+<div className="about">
+<h2 className="about-text">About</h2>
+    <p>
+      This app uses React!
+    </p>
+</div>)
+};
